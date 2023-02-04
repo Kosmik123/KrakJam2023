130e1ab1506fc2cf93fc36e8e12482251bf09beb
Donczakos <dominik.cibor@microsoft.wsei.edu.pl>
2023-02-04 02:49:55 +0100

    RotationUP&&Down
    RotationUP&&Down

--- Assets/PigScripts/CameraRotation.cs
+++ Assets/PigScripts/CameraRotation.cs
@@ -5,22 +5,37 @@ using UnityEngine;
 public class CameraRotation : MonoBehaviour
 {
     [SerializeField] Transform camera;
-
+    [SerializeField] TransformSmoother transformSmoother;
+    [SerializeField] bool UpAndDown;
     private void Update()
     {
         operationCamera();
     }
+
     public void operationCamera()
     {
 
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            camera.rotation *= Quaternion.Euler(0, -90, 0);
-
+            
+            transformSmoother.Rotation *= Quaternion.Euler(0, -90, 0);
         }
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            camera.rotation *= Quaternion.Euler(0, 90, 0);
+            transformSmoother.Rotation *= Quaternion.Euler(0, 90, 0);
+        }
+        if (UpAndDown)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+
+                transformSmoother.Rotation *= Quaternion.Euler(90, 0, 0);
+            }
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                transformSmoother.Rotation *= Quaternion.Euler(-90,0, 0);
+            }
         }
     }
+
 }
